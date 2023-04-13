9c8c7301acbb0d037d18712a8a0c2c5b0d5cade7
fjonamalo <fjonamalo0@gmail.com>
2023-04-13 18:50:46 +0200

    Async Thunks

--- app/store.js
+++ app/store.js
@@ -2,7 +2,8 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 // const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
-const { getDefaultMiddleware } = require("@reduxjs/toolkit");
+const userReducer = require("../features/user/userSlice");
+// const { getDefaultMiddleware } = require("@reduxjs/toolkit");
 
 // const logger = reduxLogger.createLogger();
 
@@ -10,8 +11,9 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
